c7e5161c90deda071d8bcfb85365eb07478fe40e
moh000 <mohmed.ayman11@gmail.com>
2024-08-04 17:50:25 +0300

    adding project files

---
+++ script.js
@@ -0,0 +1,112 @@
+"use strict";
+const RANDOM_QUOTE_API_URL = "http://api.quotable.io/random";
+const container = document.querySelector(".container");
+const timerHead = document.querySelector(".timer");
+const quote = document.querySelector(".container p");
+const textArea = document.querySelector("textarea");
+const scroeSpan = container.querySelector("span");
+const overlay = document.querySelector(".overlay");
+const message = document.querySelector(".message");
+let quoteLetters,
+  timer = 60,
+  timerCounter,
+  score = 0,
+  gameRunning = false;
+
+function getRandomQuote() {
+  return fetch(RANDOM_QUOTE_API_URL)
+    .then((response) => response.json())
+    .then((data) => data.content);
+}
+async function renderNewQuote() {
+  quote.textContent = "";
+  const quoteOnline = await getRandomQuote();
+  textArea.value = "";
+  quoteOnline.split("").forEach((character) => {
+    quote.insertAdjacentHTML("beforeend", `<span>${character}</span>`);
+  });
+  quoteLetters = quote.querySelectorAll("span");
+}
+
+
+const checkLetters = function (e) {
+  // checks if the game started or not to start the counter
+  if (!gameRunning) {
+    gameRunning = true;
+    timerCounter = setInterval(function () {
+      if (timer > 0) timerHead.textContent = --timer;
+      else stopGame();
+    }, 1000);
+  }
+  if (gameRunning) {
+    let winning = true;
+    const inputChars = textArea.value.split("");
+    quoteLetters.forEach((quoteChar, index) => {
+      if (!inputChars[index]) {
+        quoteChar.classList.remove("incorrect");
+        quoteChar.classList.remove("correct");
+        winning = false;
+      } else if (quoteChar.textContent === inputChars[index]) {
+        quoteChar.classList.add("correct");
+        quoteChar.classList.remove("incorrect");
+      } else if (quoteChar.textContent !== inputChars[index]) {
+        quoteChar.classList.add("incorrect");
+        quoteChar.classList.remove("correct");
+        winning = false;
+      }
+    });
+    if (winning) {
+      scroeSpan.textContent = ++score;
+      if (score === 2) {
+        stopGame();
+        showWinning();
+      } else init();
+    }
+  }
+};
+
+// intial state
+const init = function () {
+  timerHead.textContent = timer;
+  textArea.textContent = "";
+  textArea.focus();
+  renderNewQuote();
+};
+
+
+
+const resetCopy = (e) => {
+  e.preventDefault();
+  textArea.value = "Are You Serious?";
+};
+const showWinning = function () {
+  overlay.classList.remove("hidden");
+};
+
+const stopGame = function () {
+  clearInterval(timerCounter);
+  textArea.setAttribute("disabled", "disabled");
+};
+
+
+// prevent pasting
+textArea.addEventListener("paste", resetCopy);
+textArea.addEventListener("dragover", resetCopy);
+
+
+
+// input event
+textArea.addEventListener("input", checkLetters);
+
+
+
+// restart game on tab
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Tab") {
+    e.preventDefault();
+    window.location.reload();
+  }
+});
+
+// start game
+init();
